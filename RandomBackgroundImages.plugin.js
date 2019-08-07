f13fca97ae015c3e1f069dcafb74130b3d62ddc9
NawaNawa <ltue2129@gmail.com>
2019-08-07 14:14:36 +0800

    smoothly load, change images. v0.0.3

--- RandomBackgroundImages.plugin.js
+++ RandomBackgroundImages.plugin.js
@@ -1,6 +1,6 @@
 //META{"name":"RandomBackgroundImages","website":"https://hackmd.io/tcY9eiXHTye_6PC1jaaMgg","source":"https://github.com/NawaNae/RandomBackground"}*//
 //原始範例 : https://github.com/Metalloriff/BetterDiscordPlugins/blob/master/VoiceChatNotifications.plugin.js
-class RandomBackgroundImages {	
+var RandomBackgroundImages= class RandomBackgroundImages {	
     constructor()
     {
 
@@ -8,6 +8,7 @@ class RandomBackgroundImages {
             {src:'http://imgur.com/ljQVAD9.png',"background-size":"cover"}
         ];
         this.carousel=this.storageData("carousel");
+        this.transition=this.storageData("transition");
         this.index=0;
     }
     storageData(name)
@@ -16,13 +17,21 @@ class RandomBackgroundImages {
     }
     getName() { return "RandomBackgroundImages"; }
     getDescription() { return "Randomly select background image of body from imagelist"; }
-    getVersion() { return "0.0.1"; }
+    getVersion() { return "0.0.3"; }
 	getAuthor() { return "NawaNawa"; }
 	getChanges() {
 		return {
             "0.0.1" : 
             `
-               try to make it.
+               basic function.
+            `,
+            "0.0.2":
+            `
+            carousel function.
+            `,
+            "0.0.3":
+            `
+            smoothly load, change images.
             `
         };
     }
@@ -52,34 +61,47 @@ class RandomBackgroundImages {
         };
         for(let key in settings)
         {
-            this.info(key);
+           // this.info(key);
             if(settings[key])
                 document.body.style[key]=settings[key];
         }
     }
-    get imageUrl()
+    get imageSrc()
     {
         let img=this.img;
         switch (img.constructor.name)
         {
             case "String":
-                return this.getCssString(img);
+                return img;
             break;   
             case "Object":
                 let src=img.src||img.img||img.image;
                 if(!src)
                     this.info("random src error, object not include src/img/image property");
-                return this.getCssString(src);
+                return src;
                 break;
             default:
             break;
         }
     }
-    applyImage()
+    get imageUrl()
     {
-        document.body.style.backgroundImage=this.imageUrl;
-        if(this.img.constructor.name==="Object")
-            this.objectProcess();
+        return this.getCssString(this.imageSrc);
+    }
+    async applyImage()
+    {
+        try
+        {
+            await this.preloadImage(this.imageSrc);
+            document.body.style.backgroundImage=this.imageUrl;
+            if(this.img.constructor.name==="Object")
+                this.objectProcess();
+        }
+        catch(e)
+        {
+            ZLibrary.Modals.showAlertModal("Load image error","Can't load image from src : "+this.imageSrc+"\nPlease make sure your this url is correct and resource wasn't deleted.");
+        }
+       
     }
     nextImage()
     {
@@ -91,6 +113,17 @@ class RandomBackgroundImages {
             this.index%=this.imageList.length;
         }
         this.applyImage();
+    }
+    preloadImage(src)
+    {
+        let tmpImg=new Image();
+        let p = new Promise((resolve,reject)=>
+        {
+            tmpImg.onload=()=>{resolve(tmpImg);};
+            tmpImg.onerror=(e)=>{reject(e);};
+        });
+        tmpImg.src=src;
+        return p;
     }
     get img()
     {
@@ -114,8 +147,27 @@ class RandomBackgroundImages {
         }
         return parseInt(timeS)*timeUnitDict[timeUnit];
     }
+    cssToJsKey(key)
+    {
+        let keys=key.split("-");
+        let ansKey=keys[0];
+        for(let i=1;i<keys.length;i++)
+        {
+            ansKey+=keys[i][0].toUpperCase()+keys[i].slice(1,keys[i].length);
+        }
+        return ansKey;
+    }
+    setTransition()
+    {
+        let t=this.transition;
+        for(let key in t)
+        {
+            document.body.style[this.cssToJsKey(key)]=t[key];
+        }
+    }
     main()
     {
+        this.setTransition();
         if(!this.carousel.enable||this.carousel.random)
         {
             this.index=this.rnd();
@@ -123,6 +175,7 @@ class RandomBackgroundImages {
         this.applyImage();
         if(this.carousel.enable)
         {
+            
             this.info(this.time);
             this.timeId=window.setInterval(()=>this.nextImage(),this.time);
         }
@@ -155,5 +208,7 @@ class RandomBackgroundImages {
         {
             window.clearInterval(this.timeId);
         }
-	}
+    }
+    findVm()
+    {throw "vm";}
 }
